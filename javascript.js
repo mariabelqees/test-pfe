67ce50de0de6701cb12be229b529b81e4620aa88
marymary-s <belqeesboulefa@gmail.com>
2021-05-02 12:35:16 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var botton = document.getElementById("search-button");
+var query = document.getElementById("Ssearch-text");
+
+Button.addEventListner("click",function(){
+                                            
+} )
